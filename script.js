edfd8c310629f01b816330caf811796f5865bdb5
Sakthi002-D <sakthivel.d002@gmail.com>
2026-05-26 09:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,62 @@
-const speed = document.querySelector('.speed');
-const bar = document.querySelector('.speed-bar');
-const video = document.querySelector('.flex');
+const player = document.querySelector('.player');
+const video = document.querySelector('.player__video');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const ranges = document.querySelectorAll('.player__slider');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
-function handleMove(e) {
+function togglePlay() {
 
-    const y = e.pageY - speed.offsetTop;
-
-    const percent = y / speed.offsetHeight;
-
-    const min = 0.4;
-    const max = 4;
-
-    const height = Math.round(percent * 100) + '%';
-
-    const playbackRate = percent * (max - min) + min;
-
-    bar.style.height = height;
-
-    bar.textContent = playbackRate.toFixed(2) + '×';
-
-    video.playbackRate = playbackRate;
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
 }
 
-speed.addEventListener('mousemove', handleMove);
+function updateButton() {
+
+    const icon = video.paused ? '►' : '❚ ❚';
+
+    toggle.textContent = icon;
+}
+
+function skip() {
+
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+
+    const percent = (video.currentTime / video.duration) * 100;
+
+    progressFilled.style.flexBasis = `${percent}%`;
+}
+
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('click', togglePlay);
+
+video.addEventListener('play', updateButton);
+
+video.addEventListener('pause', updateButton);
+
+video.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach(button =>
+    button.addEventListener('click', skip)
+);
+
+ranges.forEach(range =>
+    range.addEventListener('change', handleRangeUpdate)
+);
+
+ranges.forEach(range =>
+    range.addEventListener('mousemove', handleRangeUpdate)
+);
